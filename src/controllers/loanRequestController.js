2fe34723df7f339a1e0a6f4d656353c6d812ed03
abcmehedi5 <abcmehedi5@gmail.com>
2023-09-19 03:26:42 +0600

    get singel user loan api

--- src/controllers/loanRequestController.js
+++ src/controllers/loanRequestController.js
@@ -1,12 +1,24 @@
 const connectToMongoDB = require("../Configs/db");
 
+
+// create a loan for  request loan
 const createLoan = async (loan) => {
   const client = await connectToMongoDB();
   const loanCollection = client.db("easy-loan").collection("loans");
-  const resutl = await loanCollection.insertOne(loan);
-  return resutl;
+  const result = await loanCollection.insertOne(loan);
+  return result;
 };
+
+
+// get a user loans
+const getMyLoans = async (query) =>{
+  const client = await connectToMongoDB();
+  const loanCollection = client.db("easy-loan").collection("loans");
+  const result = loanCollection.find(query).toArray()
+  return result
+}
 
 module.exports = {
   createLoan,
+  getMyLoans
 };
--- src/routes/LoanRequestRoute.js
+++ src/routes/LoanRequestRoute.js
@@ -2,10 +2,18 @@ const express = require("express");
 const router = express.Router();
 const loanRequestController = require("../controllers/loanRequestController");
 
+// create a loan for  request loan
 router.post("/loan", async (req, res) => {
   const loan = req.body;
   await loanRequestController.createLoan(loan);
   res.status(200).send({ message: "loan request successfull" });
 });
 
+// get a single user loans
+router.get("/loan", async (req, res) => {
+  const email = req.query.email;
+  const query = { email: email };
+  const result = await loanRequestController.getMyLoans(query);
+  res.status(200).send(result);
+});
 module.exports = router;
