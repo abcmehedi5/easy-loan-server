a775827b007973c879c88768add69304e13f5a41
abcmehedi5 <abcmehedi5@gmail.com>
2023-09-19 23:18:50 +0600

    loan request api

--- src/controllers/loanRequestController.js
+++ src/controllers/loanRequestController.js
@@ -1,6 +1,5 @@
 const connectToMongoDB = require("../Configs/db");
 
-
 // create a loan for  request loan
 const createLoan = async (loan) => {
   const client = await connectToMongoDB();
@@ -9,16 +8,34 @@ const createLoan = async (loan) => {
   return result;
 };
 
-
 // get a user loans
-const getMyLoans = async (query) =>{
+const getMyLoans = async (query) => {
   const client = await connectToMongoDB();
   const loanCollection = client.db("easy-loan").collection("loans");
-  const result = loanCollection.find(query).toArray()
-  return result
-}
+  const result = loanCollection.find(query).toArray();
+  return result;
+};
+// get a user scheduel loan
+const loanScheduled = async (query) => {
+  const client = await connectToMongoDB();
+  const loanCollection = client.db("easy-loan").collection("loans");
+  const result = loanCollection.findOne(query);
+  return result;
+};
+
+// repayment
+
+const repayment = async (query ,updateDoc) => {
+  console.log({naem: updateDoc});
+  const client = await connectToMongoDB();
+  const loanCollection = client.db("easy-loan").collection("loans");
+  const result = await loanCollection.updateOne(query ,updateDoc);
+  return result;
+};
 
 module.exports = {
   createLoan,
-  getMyLoans
+  getMyLoans,
+  loanScheduled,
+  repayment
 };
--- src/routes/LoanRequestRoute.js
+++ src/routes/LoanRequestRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const loanRequestController = require("../controllers/loanRequestController");
+const { ObjectId } = require("mongodb");
 
 // create a loan for  request loan
 router.post("/loan", async (req, res) => {
@@ -15,5 +16,34 @@ router.get("/loan", async (req, res) => {
   const query = { email: email };
   const result = await loanRequestController.getMyLoans(query);
   res.status(200).send(result);
+});
+
+// single loan get
+
+router.get("/loan-scheduled/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = {
+    _id: new ObjectId(id),
+  };
+  const response = await loanRequestController.loanScheduled(query);
+  res.status(200).send(response);
+});
+
+// repayment router
+
+router.patch("/repayment/:id", async (req, res) => {
+  const amount = req.body.loanAmount;
+  console.log(amount);
+  const id = req.params.id;
+  console.log(amount, id);
+  const query = { _id: new ObjectId(id) };
+  const updateDoc = {
+    $inc: {
+      loanAmount: -amount,
+      dividedLoan: -1,
+    },
+  };
+  await loanRequestController.repayment(query, updateDoc);
+  res.status(200).send({ message: "Payment Successfull" });
 });
 module.exports = router;
